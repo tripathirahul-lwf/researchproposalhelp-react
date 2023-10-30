83f2671ad1e28af3691b8a27e0c6c7ddf06d2cb2
Rahul Tripathi <rahullearnwithfraternity@gmail.com>
2023-10-30 15:44:49 +0530

    form

--- src/component/form/Form.jsx
+++ src/component/form/Form.jsx
@@ -1,21 +1,18 @@
 import React, { useEffect } from 'react'
 
 const Form = () => {
-    useEffect(() =>{
-        const script = document.createElement('script');
-        script.src = "https://forms.app/static/embed.js"
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://forms.app/static/embed.js";
         script.async = true;
 
         document.body.appendChild(script);
 
-        script.onload = () =>{
-            var formsappForm = new window.formsapp('65117e3a5e2b7cb187f19c02', 'standard', { "width": "100vw", "height": "500px" })
+        script.onload = () => {
+            var formsappForm = new window.formsapp('64edf9f987b11fad18feb978', 'standard', { "width": "100%", "height": "500px", "opacity": 0 });
         }
-    },[])
-
-    return (
-        <div formsappId="65117e3a5e2b7cb187f19c02"></div>
-    )
+    }, [])
+    return <div formsappId="64edf9f987b11fad18feb978" className='w-80'></div>;
 }
 
 export default Form
